c8b399ee83eb47dd4512188139d733e3bf8119dc
Phillip Jones <ptj92e@gmail.com>
2020-05-15 12:08:09 -0500

    Linked React with Server

---
+++ server/static/react/precache-manifest.cd4d771c54869f2d1ce5221bffb76b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c65a354f13ae1db8d3076815089afd",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "36551846fcc6ed53203d",
+    "url": "/static/react/css/main.538a5ac4.chunk.css"
+  },
+  {
+    "revision": "ce8642e2f26d4fad28d1",
+    "url": "/static/react/js/2.bd97f94e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.bd97f94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36551846fcc6ed53203d",
+    "url": "/static/react/js/main.0f115eda.chunk.js"
+  },
+  {
+    "revision": "9ed2299df9314efa0b61",
+    "url": "/static/react/js/runtime-main.0e36708b.js"
+  }
+]);
