33b852bec3bc5fc3d185fdf28a21f76276c1d9e5
Phillip Jones <ptj92e@gmail.com>
2020-05-21 19:07:38 -0500

    Added dotenv

--- server/main.py
+++ server/main.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from dotenv import load_dotenv
 import pymysql
 import secrets
 import os
 from datetime import datetime
 
-conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+load_dotenv(verbose=True)
+
+USER = os.getenv("dbuser")
+PASSWORD = os.getenv("dbpass")
+HOST = os.getenv("dbhost")
+NAME = os.getenv("dbname")
+
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(USER, PASSWORD, HOST, NAME)
 
 app = Flask("__name__")
 bcrypt = Bcrypt(app)
